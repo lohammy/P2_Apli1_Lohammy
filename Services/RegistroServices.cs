7bf39a48edeff9c426e4a48d191077d502537428
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 18:31:09 -0400

    cree el services

---
+++ Services/RegistroServices.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using P2_Apli1_Lohammy.DAL;
+using P2_Apli1_Lohammy.Models;
+
+namespace P2_Apli1_Lohammy.Services;
+
+public class RegistroServices (IDbContextFactory<Contexto> DbFactory)
+{
+
+    public async Task<bool> Guardar(Registro registro)
+    {
+        if (!await Existe(registro.IdEntrada))
+        {
+            return await Insertar(registro);
+        }
+        else
+        {
+            return await Modificar(registro);
+        }
+    }
+    private async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registro.AnyAsync(p => p.IdEntrada == id);
+    }
+
+    private async Task<bool> Insertar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Registro.Add(registro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(Registro registro)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        var entradaAnterior = await contexto.Registro
+            .Include(e => e.entradaHuacalesDetalle)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.IdEntrada == registro.IdEntrada);
+
+        if (entradaAnterior == null)
+        {
+            return false;
+        }
+
+        await AfectarEntradasHuacales(detalle: [.. entradaAnterior.entradaHuacalesDetalle],
+                                      TipoOperacion.Resta);
+
+        await AfectarEntradasHuacales([.. registro.entradaHuacalesDetalle], TipoOperacion.Suma);
+
+        contexto.Registro.Update(registro);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
+    {
+
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registro
+            .Include(e => e.entradaHuacalesDetalle)
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
