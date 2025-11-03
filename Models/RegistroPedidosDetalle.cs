de5a694f80db253ac260bd9c2af3165c2d47f5e0
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 19:27:30 -0400

    agregue las entidades

--- Models/RegistroPedidosDetalle.cs
+++ Models/RegistroPedidosDetalle.cs
@@ -1,6 +1,31 @@
-﻿namespace P2_Apli1_Lohammy.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P2_Apli1_Lohammy.Models;
+
+public class RegistroPedidosDetalle
 {
-    public class RegistroPedidosDetalle
-    {
-    }
+    [Key]
+
+    public int Id { get; set; }
+    public int PedidoId { get; set; }
+    public int ComponenteId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir una cantidad valida")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir un monto valido")]
+    public decimal Precio { get; set; }
+
+    [ForeignKey("PedidoId")]
+    [InverseProperty("registroPedidosDetalle")]
+    public virtual RegistroPedidos? registroPedidos { get; set; } = null;
+
+
+    //[ForeignKey("ComponenteId")]
+    //[InverseProperty("RegistroPedidosDetalle")]
+
+    //public virtual TiposHuacales tiposHuacales { get; set; }
 }
