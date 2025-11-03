166a0081c47fc82bfca5b61d236f73623463153f
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 18:32:09 -0400

    agg la base de datos

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var ConStr = builder.Configuration.GetConnectionString("sqlConnection");
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
