e2820103f46fe2f3bdc9836748c14ebab45924a4
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 19:27:00 -0400

    cree componentes

---
+++ Models/Componentes.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace P2_Apli1_Lohammy.Models
+{
+    public class Componente
+    {
+        [PrimaryKey]
+        public int ComponenteId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, double.MaxValue, ErrorMessage = "Debe introducir una cantidad valida")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, double.MaxValue, ErrorMessage = "Debe introducir una cantidad valida")]
+        public int Existencia { get; set; }
+    }
+}
