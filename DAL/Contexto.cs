c63637b9b020813b72d2e5d57fca28d0329b3865
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 19:26:47 -0400

    inyecte el modelo

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,9 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using P2_Apli1_Lohammy.Models;
 
 namespace P2_Apli1_Lohammy.DAL;
 
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
-
+    public DbSet<Componente> Componentes { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Componente>(entity =>
+        {
+            entity.HasData(
+            new Componente
+            {
+                ComponenteId = 1,
+                Descripcion = "Memoria 4GB",
+                Precio = 1580,
+                Existencia = 1
+            },
+            new Componente
+            {
+                ComponenteId = 2,
+                Descripcion = "Disco SSD 120MB",
+                Precio = 4200,
+                Existencia = 8
+            },
+            new Componente
+            {
+                ComponenteId = 3,
+                Descripcion = "Tarjeta de Video",
+                Precio = 10000,
+                Existencia = 4
+            });
+        });
+    }
 }
