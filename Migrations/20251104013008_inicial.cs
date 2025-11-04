d9e5e18346a89376dae9118512584f8f5dd265d9
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 21:35:58 -0400

    migracion

---
+++ Migrations/20251104013008_inicial.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace P2_Apli1_Lohammy.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Componentes",
+                columns: table => new
+                {
+                    ComponenteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Componentes", x => x.ComponenteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegistroPedidos",
+                columns: table => new
+                {
+                    PedidoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    NombreCliente = table.Column<string>(type: "TEXT", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistroPedidos", x => x.PedidoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegistroPedidosDetalle",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PedidoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ComponenteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistroPedidosDetalle", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RegistroPedidosDetalle_Componentes_ComponenteId",
+                        column: x => x.ComponenteId,
+                        principalTable: "Componentes",
+                        principalColumn: "ComponenteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RegistroPedidosDetalle_RegistroPedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "RegistroPedidos",
+                        principalColumn: "PedidoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Componentes",
+                columns: new[] { "ComponenteId", "Descripcion", "Existencia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, "Memoria 4GB", 1, 1580m },
+                    { 2, "Disco SSD 120MB", 8, 4200m },
+                    { 3, "Tarjeta de Video", 4, 10000m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistroPedidosDetalle_ComponenteId",
+                table: "RegistroPedidosDetalle",
+                column: "ComponenteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistroPedidosDetalle_PedidoId",
+                table: "RegistroPedidosDetalle",
+                column: "PedidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegistroPedidosDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Componentes");
+
+            migrationBuilder.DropTable(
+                name: "RegistroPedidos");
+        }
+    }
+}
