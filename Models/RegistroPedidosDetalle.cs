011acd50a545ba2a13794acb73f8b669cc3f124f
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 21:37:54 -0400

    quite comentarios

--- Models/RegistroPedidosDetalle.cs
+++ Models/RegistroPedidosDetalle.cs
@@ -1,31 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace P2_Apli1_Lohammy.Models;
-
-public class RegistroPedidosDetalle
+namespace P2_Apli1_Lohammy.Models
 {
-    [Key]
+    public class RegistroPedidosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
 
-    public int Id { get; set; }
-    public int PedidoId { get; set; }
-    public int ComponenteId { get; set; }
+        public int PedidoId { get; set; }
+        public int ComponenteId { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio")]
-    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir una cantidad valida")]
-    public int Cantidad { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, double.MaxValue, ErrorMessage = "Debe introducir una cantidad valida")]
+        public int Cantidad { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio")]
-    [Range(1, double.MaxValue, ErrorMessage = "Debe introducir un monto valido")]
-    public decimal Precio { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1, double.MaxValue, ErrorMessage = "Debe introducir un monto valido")]
+        public decimal Precio { get; set; }
 
-    [ForeignKey("PedidoId")]
-    [InverseProperty("registroPedidosDetalle")]
-    public virtual RegistroPedidos? registroPedidos { get; set; } = null;
+        [ForeignKey("PedidoId")]
+        [InverseProperty("PedidosDetalles")]
+        public virtual RegistroPedidos RegistroPedido { get; set; } = null;
 
-
-    //[ForeignKey("ComponenteId")]
-    //[InverseProperty("RegistroPedidosDetalle")]
-
-    //public virtual TiposHuacales tiposHuacales { get; set; }
+        [ForeignKey("ComponenteId")]
+        [InverseProperty("PedidosDetalles")]
+        public virtual Componente Componentes { get; set; }
+    }
 }
