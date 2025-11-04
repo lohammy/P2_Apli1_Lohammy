536add10db2cb2b2bf4b512080ae033d5ce2a6d7
lohammy vasquez adames <2005lohammy@gmail.com>
2025-11-03 21:37:35 -0400

    agregue inverseproperty

--- Models/RegistroPedidos.cs
+++ Models/RegistroPedidos.cs
@@ -1,17 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
-namespace P2_Apli1_Lohammy.Models;
-
-public class RegistroPedidos
+namespace P2_Apli1_Lohammy.Models
 {
-    [Key] 
-    public int PedidoId { get; set; }
+    public class RegistroPedidos
+    {
+        [Key]
+        public int PedidoId { get; set; }
 
-    [Required(ErrorMessage = "Este campo es obligatorio")]
-    public DateTime Fecha { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Esto es obligatorio")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
 
-    [Required(ErrorMessage = "Este campo es obligatorio")]
-    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
-    public string NombreCliente { get; set; }
-    public decimal Total { get; set; }
+        [Required(ErrorMessage = "Esto es obligatorio")]
+        [RegularExpression("^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", ErrorMessage = "En este campo solo se permiten letras.")]
+        public string NombreCliente { get; set; }
+
+        public decimal Total { get; set; }
+
+        [InverseProperty("RegistroPedido")]
+        public virtual ICollection<RegistroPedidosDetalle> PedidosDetalles { get; set; } = [];
+    }
 }
